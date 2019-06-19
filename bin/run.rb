cc095f6626e65d0982514f27ef52d94222955271
github username <github email address>
2019-06-19 10:55:15 -0400

    added models to the run folder

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,8 @@
 require_relative '../config/environment'
+require_relative '../app/models/game_round'
+require_relative '../app/models/movie'
+require_relative '../app/models/question'
+require_relative '../app/models/user.rb'
+#require_relative '../app/AppController_or_Cli' <= to run the Cli
 
 puts "Welcome to the MOVIE GAME"
