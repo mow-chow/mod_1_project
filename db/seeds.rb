830e670a807128c9945035a702573065f2f0fea3
github username <github email address>
2019-06-18 20:49:13 -0400

    added movie seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,83 @@ title: "",
 plot: "", 
 actors: []
 )
+
+Movie.create(
+title: "The Martian", 
+plot: "An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
+actors: ["Matt Damon", " Jessica Chastain", " Kristen Wig", " Jeff Daniels", " Michael Pena", " Sean Bean", " Kate Mara", " Sebastian Stan", " Chiwetel Ejiofor", " Donald Glover"] 
+)
+
+
+Movie.create(
+title: "Armageddon", 
+plot: "After discovering that an asteroid the size of Texas is going to impact Earth in less than a month, N.A.S.A. recruits a misfit team of deep core drillers to save the planet.", 
+actors: ["Bruce Willis", " Billy Bob Thornton", " Ben Affleck", " Liv Tyler", " Steve Buscemi", " Owen Wilson", " Michael Clarke Duncan"]
+)
+
+Movie.create(
+title: "War of the Worlds"
+plot: "After discovering that an asteroid the size of Texas is going to impact Earth in less than a month, N.A.S.A. recruits a misfit team of deep core drillers to save the planet.", 
+actors: ["Tom Cruise", "Dakota Fanning", "Tim Robbins", "Justin Chatwin"]
+)
+
+Movie.create(
+title: "The Lord of The Rings", 
+plot: "The future of civilization rests in the fate of the One Ring, which has been lost for centuries. Powerful forces are unrelenting in their search for it. But fate has placed it in the hands of a young Hobbit named Frodo Baggins, who inherits the Ring and steps into legend. A daunting task lies ahead for Frodo when he becomes the Ringbearer - to destroy the One Ring in the fires of Mount Doom where it was forged.", 
+actors: ["Elijah Wood", "Ian McKellen",  "Liv Tyler", "Viggo Mortensen", "Sean Astin", "Cate Blanchett", "John Rhys-Davies", "Billy Boyd", "Dominic Monaghan", "Orlando Bloom", "Christopher Lee", "Hugo Weaving", "Sean Bean", "Ian Holm", "Andy Serkis"]
+)
+
+Movie.create(
+title: "Star Wars", 
+plot: "Three decades after the Empire's defeat, a new threat arises in the militant First Order. Defected stormtrooper Finn and the scavenger Rey are caught up in the Resistance's search for the missing Luke Skywalker", 
+actors: ["Harrison Ford", " Mark Hamill", " Carrie Fisher", " Adam Driver", " Daisy Ridley", " John Boyega", " Oscar Isaac", " Lupita Nyong'o", " Andy Serkis", " Domhnall Gleeson", " Anthony Davis", " Gwendoline Christie"]
+)
+
+Movie.create(
+title: "The Avengers", 
+plot: "When Thor's evil brother, Loki, gains access to the unlimited power of the energy cube called the Tesseract, Nick Fury, director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth.", 
+actors: ["Robert Downey Jr. ", " Chris Evans", " Mark Ruffalo", " Chris Hemsworth", " Scarlett Johansson", " Jeremy Renner", " Tom Hiddleston", " Clark Greg", " Cobie Smulders", " Stellan Skarsård", " Samuel L. Jackson"]
+)
+
+Movie.create(
+title: "The Departed", 
+plot: "inspired by the wildly popular 2002 Hong Kong crime film Infernal Affairs. As the police force attempts to reign in the increasingly powerful Irish mafia, authorities are faced with the prospect of sending in an undercover agent or seeing their already frail grip on the criminal underworld slip even further. ", 
+actors: ["Leonardo DiCaprio", " Matt Damon", " Jack Nicholson", " Mark Wahlberg", " Vera Farmiga", " Alec Baldwin", " Martin Sheen", " Anthony Anderson"]
+)
+
+Movie.create(
+title: "Harry Potter" , 
+plot: "An orphaned magical boy juggles a rigerious class schedule and defeating his parent's murder", 
+actors: ["Daniel Radcliffe", " Rupert Grint", " Emma Watson", " Maggie Smith", " Alan Rickman", " Richard Harris", " Michael Gambon"]
+)
+
+Movie.create(
+title: "Mad Max", 
+plot: "In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter", 
+actors: ["Tom Hardy", " Charlize Theron", " Nicholas Hoult", " Zoe Kravitz"]
+)
+
+Movie.create(
+title: "Zombieland", 
+plot: "A shy student trying to reach his family in Ohio, a gun-toting tough guy trying to find the last Twinkie, and a pair of sisters trying to get to an amusement park join forces to travel across a zombie-filled America.", 
+actors: ["Jeese Eisenberg", " Woody Harrelson", " Emma Stone", " Abigail Breslin", " Bill Murray"] 
+)
+
+Movie.create(
+title: "The Matrix" , 
+plot: "The franchise follows a group of heroes who fight a desperate war against machine overlords that have enslaved humanity in an extremely sophisticated virtual reality system.", 
+actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Jada Pinkett Smith", "Hugo Weaving"]
+)
+
+Movie.create(
+title: "The Hunger Games", 
+plot: "Every citizen must watch as the youths fight to the death until only one remains. ", 
+actors: ["Jennifer Lawrence", "Josh Hutcherson", "Liam Hemsworth", "Woody Harrelson", "Elizabeth Banks","Donald Sutherland", "Stanley Tucci", "Philip Seymour Hoffman"]
+)
+
+
+# Movie.create(
+# title: , 
+# plot: "", 
+# actors: 
+# )
